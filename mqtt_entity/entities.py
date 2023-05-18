759753ed2074da576e76547154c0140a3046b4c0
Johann Kellerman <kellerza@gmail.com>
2023-05-18 22:11:20 +0200

    attrs

--- mqtt_entity/entities.py
+++ mqtt_entity/entities.py
@@ -3,7 +3,7 @@ import inspect
 import logging
 from typing import Any, Callable, Optional, Sequence, Union
 
-import attr
+import attrs
 from attrs import validators
 
 from mqtt_entity.utils import required
@@ -13,21 +13,21 @@ _LOGGER = logging.getLogger(__name__)
 # pylint: disable=too-few-public-methods, too-many-instance-attributes
 
 
-@attr.define
+@attrs.define
 class Device:
     """A Home Assistant Device, used to group entities."""
 
-    identifiers: list[Union[str, tuple[str, Any]]] = attr.field(
+    identifiers: list[Union[str, tuple[str, Any]]] = attrs.field(
         validator=[validators.instance_of(list), validators.min_len(1)]
     )
-    connections: list[str] = attr.field(factory=list)
-    configuration_url: str = attr.field(default="")
-    manufacturer: str = attr.field(default="")
-    model: str = attr.field(default="")
-    name: str = attr.field(default="")
-    suggested_area: str = attr.field(default="")
-    sw_version: str = attr.field(default="")
-    via_device: str = attr.field(default="")
+    connections: list[str] = attrs.field(factory=list)
+    configuration_url: str = attrs.field(default="")
+    manufacturer: str = attrs.field(default="")
+    model: str = attrs.field(default="")
+    name: str = attrs.field(default="")
+    suggested_area: str = attrs.field(default="")
+    sw_version: str = attrs.field(default="")
+    via_device: str = attrs.field(default="")
 
     @property
     def id(self) -> str:  # pylint: disable=invalid-name
@@ -35,34 +35,34 @@ class Device:
         return str(self.identifiers[0])
 
 
-@attr.define
+@attrs.define
 class Availability:
     """Represent Home Assistant entity availability."""
 
-    topic: str = attr.field()
-    payload_available: str = attr.field(default="online")
-    payload_not_available: str = attr.field(default="offline")
-    value_template: str = attr.field(default="")
+    topic: str = attrs.field()
+    payload_available: str = attrs.field(default="online")
+    payload_not_available: str = attrs.field(default="offline")
+    value_template: str = attrs.field(default="")
 
 
-@attr.define
+@attrs.define
 class Entity:
     """A generic Home Assistant entity used as the base class for other entities."""
 
-    unique_id: str = attr.field()
-    device: Device = attr.field()
-    state_topic: str = attr.field()
-    name: str = attr.field()
-    availability: list[Availability] = attr.field(factory=list)
-    availability_mode: str = attr.field(default="")
-    device_class: str = attr.field(default="")
-    unit_of_measurement: str = attr.field(default="")
-    state_class: str = attr.field(default="")
-    expire_after: int = attr.field(default=0)
+    unique_id: str = attrs.field()
+    device: Device = attrs.field()
+    state_topic: str = attrs.field()
+    name: str = attrs.field()
+    availability: list[Availability] = attrs.field(factory=list)
+    availability_mode: str = attrs.field(default="")
+    device_class: str = attrs.field(default="")
+    unit_of_measurement: str = attrs.field(default="")
+    state_class: str = attrs.field(default="")
+    expire_after: int = attrs.field(default=0)
     """Unavailable if not updated."""
-    enabled_by_default: bool = attr.field(default=True)
-    entity_category: str = attr.field(default="")
-    icon: str = attr.field(default="")
+    enabled_by_default: bool = attrs.field(default=True)
+    entity_category: str = attrs.field(default="")
+    icon: str = attrs.field(default="")
 
     _path = ""
 
@@ -77,12 +77,12 @@ class Entity:
     def asdict(self) -> dict:
         """Represent the entity as a dictionary, without empty values and defaults."""
 
-        def _filter(atrb: attr.Attribute, value: Any) -> bool:
+        def _filter(atrb: attrs.Attribute, value: Any) -> bool:
             return (
                 bool(value) and atrb.default != value and not inspect.isfunction(value)
             )
 
-        return attr.asdict(self, filter=_filter)
+        return attrs.asdict(self, filter=_filter)
 
     @property
     def topic(self) -> str:
@@ -93,64 +93,64 @@ class Entity:
         return f"homeassistant/{self._path}/{did}/{uid}/config"
 
 
-@attr.define
+@attrs.define
 class SensorEntity(Entity):
     """A Home Assistant Sensor entity."""
 
     _path = "sensor"
 
 
-@attr.define
+@attrs.define
 class BinarySensorEntity(Entity):
     """A Home Assistant Binary Sensor entity."""
 
-    payload_on: str = attr.field(default="ON")
-    payload_off: str = attr.field(default="OFF")
+    payload_on: str = attrs.field(default="ON")
+    payload_off: str = attrs.field(default="OFF")
 
     _path = "binary_sensor"
 
 
-@attr.define
+@attrs.define
 class RWEntity(Entity):
     """Read/Write entity base class.
 
     This will default to a text entiry."""
 
-    command_topic: str = attr.field(
+    command_topic: str = attrs.field(
         default="", validator=(validators.instance_of(str), validators.min_len(2))
     )
 
-    on_change: Optional[Callable] = attr.field(default=None)
+    on_change: Optional[Callable] = attrs.field(default=None)
 
     _path = "text"
 
 
-@attr.define
+@attrs.define
 class SelectEntity(RWEntity):
     """A HomeAssistant Select entity."""
 
-    options: Sequence[str] = attr.field(default=None, validator=required)
+    options: Sequence[str] = attrs.field(default=None, validator=required)
 
     _path = "select"
 
 
-@attr.define
+@attrs.define
 class SwitchEntity(RWEntity):
     """A Home Assistant Binary Sensor entity."""
 
-    payload_on: Union[str, int] = attr.field(default=1)
-    payload_off: Union[str, int] = attr.field(default=0)
+    payload_on: Union[str, int] = attrs.field(default=1)
+    payload_off: Union[str, int] = attrs.field(default=0)
 
     _path = "switch"
 
 
-@attr.define
+@attrs.define
 class NumberEntity(RWEntity):
     """A HomeAssistant Number entity."""
 
-    min: float = attr.field(default=0.0)
-    max: float = attr.field(default=100.0)
-    mode: str = attr.field(default="auto")
-    step: float = attr.field(default=1.0)
+    min: float = attrs.field(default=0.0)
+    max: float = attrs.field(default=100.0)
+    mode: str = attrs.field(default="auto")
+    step: float = attrs.field(default=1.0)
 
     _path = "number"
